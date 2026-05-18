944378b0f344135befaba22a60f854b8ebebb89c
Saanvi Sood <saanvii.sood@gmail.com>
2026-05-18 12:36:03 -0400

    unified naming conventions

--- main.tf
+++ main.tf
@@ -23,16 +23,16 @@ provider "aws" {
 }
 
 variable "open_webui_user" {
-  description = "Username to access the web UI"
+  description = "Username to access Open WebUI"
   default     = "admin@demo.gs"
 }
 
 variable "openai_base" {
-  description = "Optional base URL to use OpenAI API with Open Web UI"
+  description = "Optional base URL to use OpenAI API with Open WebUI"
   default     = "https://api.openai.com/v1"
 }
 
 variable "openai_key" {
-  description = "Optional API key to use OpenAI API with Open Web UI"
+  description = "Optional API key to use OpenAI API with Open WebUI"
   default     = ""
 }
